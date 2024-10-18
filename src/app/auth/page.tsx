759519280855155f2a4171207240152512f4b065
edwxrdbobb <edwardbobkamara@gmail.com>
2024-10-18 17:39:24 +0000

    Auth page.tsx fixed

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,17 @@
+// import Login from "./login/page"
+
+
+const Auth = () => {
+
+    return(
+        <div>
+            <h1 className="text-black text-2xl">Welcome to UN Women</h1>
+            <p className="text-black">what is your next action?</p>
+
+            <a href="/auth/login"><button className="">Login</button></a>
+           
+        </div>
+    )
+}
+
+export default Auth
