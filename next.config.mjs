7ec3e7e5efece6fdcbf5a5a56983a22a859f719e
edwxrdbobb <edwardbobkamara@gmail.com>
2024-12-05 13:30:42 +0000

    change type check and lint check in config files.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
 eslint: {
     ignoreDuringBuilds: true,
-  }
+    ignoreBuildErrors: true
+  },
+typescript: {
+    ignoreBuildErrors: true,
+    ignoreDuringBuilds: true
+  },
 };
 
 export default nextConfig;
